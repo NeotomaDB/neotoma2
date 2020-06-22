6b29bc6d3c0e078f4bdcab8df17674406aa601be
Simon Goring <goring@wisc.edu>
2020-06-22 15:48:53 -0700

    Adding a site class for the package.

---
+++ R/site.R
@@ -0,0 +1,16 @@
+#' An S4 class for site information from the Neotoma Paleoecology Database.
+#'
+#' @importFrom sf sf
+
+site <- setClass("site",
+  representation(siteid = "numeric",
+               sitename = "character",
+               location = "sf",
+               description = "character",
+               notes = "character"),
+             prototype(siteid = NA_integer_,
+               sitename = NA_character_,
+               location = NULL,
+               description = NA_character_,
+               notes = NA_character_),
+             validity = function(x) {!is.na(x@sitename) & !is.na(x@siteid)})
