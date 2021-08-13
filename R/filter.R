2ad26180bf7350c609f51b2becb602fdca11c268
sedv8808 <sedv8808@gmail.com>
2021-08-12 23:49:07 -0700

    Filter function using dplyr and eval
    New filter function access all items using >, < or == or !=

--- R/filter.R
+++ R/filter.R
@@ -2,6 +2,7 @@
 #' @import sf
 #' @import dplyr
 #' @import purrr
+#' @import stringr
 #' @param x A site, dataset or download.
 #' @param ... arguments to filter by: latmin, latmax, longmin, longmax, elevmin, elevmax, datasettype.
 #' @export
@@ -12,16 +13,40 @@ filter <- function(x, ...) {
 
 #' @export
 filter.default <- function(x, ...) {
-  
+
+  # Naming the Dots
   cl <- as.list(match.call())
-  cl[[1]] <- NULL
-  cl <- lapply(cl, eval, envir = parent.frame())
+  cl[1] <- NULL
+  cl$x <- NULL
   
-  error_check <- check_args(cl)
+  calls_list <- c()
+  for(i in 1:length(cl)){
+    txt <- paste0(cl[[i]], collapse=', ')
+    if(str_detect(txt, 'lat')){
+      # Appending latitude call
+      my_list = list(lat = cl[[i]])
+      calls_list = append(calls_list, my_list)
+    }
+    if(str_detect(txt, 'long')){
+      # Appending longitude call
+      my_list = list(long = cl[[i]])
+      calls_list = append(calls_list, my_list)
+    }
+    if(str_detect(txt, 'elev')){
+      # Appending elevation call
+      my_list = list(elev = cl[[i]])
+      calls_list = append(calls_list, my_list)
+    }
+    if(str_detect(txt, 'datasettype')){
+      # Appending datasettype call
+      my_list = list(datasettype = cl[[i]])
+      calls_list = append(calls_list, my_list)
+    }
+  }
   
-  for(i in as.list(names(cl))){
-    if(!(i %in% c('latmin', 'latmax', 'longmin', 'longmax', 'elevmin', 'elevmax', 'datasettype', 'x')))
-      message(paste0(i, " not a valid parameter. Will not be used for filtering"))
+  for(i in names(calls_list)){
+    if(!(i %in% c('lat', 'long', 'elev', 'datasettype', 'loc')))
+      message(paste0(i, " is not a valid parameter. It will not be used for filtering"))
   }
   sites <- c()
   
@@ -31,119 +56,85 @@ filter.default <- function(x, ...) {
       
       datasets_list <- c()
       datasets <- c()
+      
       for(k in 1:length(x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets)){
-        
-        datasets_latmin <- c()
-        datasets_latmax <- c()
-        datasets_longmin <- c()
-        datasets_longmax <- c()
-        datasets_elevmin <- c()
-        datasets_elevmax <- c()
+        datasets_lat <- c()
+        datasets_long <- c()
+        datasets_elev <- c()
         datasets_type <- c()
-
         # Check for type
-        if('datasettype' %in% names(cl)){
-          if(x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]@datasettype == cl$datasettype){
-            dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
+        if('datasettype' %in% names(calls_list)){
+          datasettype <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]@datasettype
+          dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
+          if(eval(calls_list$datasettype) == TRUE){
             datasets_type <- append(datasets_type, dataset)
+          }else{
+            datasets_type <- append(datasets_type, 0)
           }
         }
         
-        # Check for latmin
-        if('latmin' %in% names(cl)){
-          if(st_coordinates(x@sites[[i]]@location)[,2][1] > cl$latmin){
-            dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
-            datasets_latmin <- append(datasets_latmin, dataset)
+        # Check for lat
+        if('lat' %in% names(calls_list)){
+          lat <- st_coordinates(x@sites[[i]]@location)[,2][1]
+          dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
+          if(eval(calls_list$lat) == TRUE){
+            datasets_lat <- append(datasets_lat, dataset)
           }else{
-            datasets_latmin <- append(datasets_latmin, 0)
+          datasets_lat <- append(datasets_lat, 0)
           }
         }
-        # Check for latmax
-        if('latmax' %in% names(cl)){
-          if(st_coordinates(x@sites[[i]]@location)[,2][1] < cl$latmax){
-            dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
-            datasets_latmax <- append(datasets_latmax, dataset)
+          
+        # Check for long
+        if('long' %in% names(calls_list)){
+          long <- st_coordinates(x@sites[[i]]@location)[,1][1]
+          dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
+          if(eval(calls_list$long) == TRUE){
+            datasets_long <- append(datasets_long, dataset)
           }else{
-            datasets_latmax <- append(datasets_latmax, 0)
+            datasets_long <- append(datasets_long, 0)
           }
         }
-        # Check for longmin
-        if('longmin' %in% names(cl)){
-          if(st_coordinates(x@sites[[i]]@location)[,1][1] > cl$longmin){
-            dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
-            datasets_longmin <- append(datasets_longmin, dataset)
+          
+        # Check for elev
+        if('elev' %in% names(calls_list)){
+          elev <- x@sites[[i]]@altitude
+          dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
+          if(eval(calls_list$elev) == TRUE){
+            datasets_elev <- append(datasets_elev, dataset)
           }else{
-            datasets_longmin <- append(datasets_longmin, 0)
-          }
-        }
-        # Check for longmax
-        if('longmax' %in% names(cl)){
-          if(st_coordinates(x@sites[[i]]@location)[,1][1] < cl$longmax){
-            dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
-            datasets_longmax <- append(datasets_longmax, dataset)
-          }else{
-            datasets_longmax <- append(datasets_longmax, 0)
-          }
-        }
-        # Check for elevgmin
-        if('elevmin' %in% names(cl)){
-          if(x@sites[[i]]@altitude > cl$elevmin){
-            dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
-            datasets_elevmin <- append(datasets_elevmin, dataset)
-          }else{
-            datasets_elevmin <- append(datasets_elevmin, 0)
-          }
-        }
-        # Check for elevmax
-        if('elevmax' %in% names(cl)){
-          if(x@sites[[i]]@altitude < cl$elevmax){
-            dataset <- x@sites[[i]]@collunits@collunits[[j]]@datasets@datasets[[k]]
-            datasets_elevmax <- append(datasets_elevmax, dataset)
-          }else{
-            datasets_elevmax <- append(datasets_elevmax, 0)
+            datasets_elev <- append(datasets_elev, 0)
           }
         }
         
-        
-        datasets_names <- list(latmin = datasets_latmin, 
-                               latmax = datasets_latmax, 
-                               longmin = datasets_longmin, 
-                               longmax = datasets_longmax, 
-                               elevmin = datasets_elevmin, 
-                               elevmax = datasets_elevmax,
+        # TODO: Check for loc element
+        datasets_names <- list(lat = datasets_lat,  
+                               long = datasets_long, 
+                               elev = datasets_elev, 
                                datasettype = datasets_type)
         
-        if(!("datasettype" %in% names(cl))){
-          datasets_names <- purrr::list_modify(datasets_names, "datasettype" = NULL)
+        if(!("datasettype" %in% names(calls_list))){
+          datasets_names$datasettype <- NULL
+          }
+        
+        if(!("lat" %in% names(calls_list))){
+          datasets_names$lat <- NULL
+          }
+        
+        if(!("long" %in% names(calls_list))){
+          datasets_names <- purrr::list_modify(datasets_names, "long" = NULL)
         }
-        if(!("latmin" %in% names(cl))){
-          datasets_names <- purrr::list_modify(datasets_names, "latmin" = NULL)
-        }
-        if(!("latmax" %in% names(cl))){
-          datasets_names <- purrr::list_modify(datasets_names, "latmax" = NULL)
-        }
-        if(!("longmin" %in% names(cl))){
-          datasets_names <- purrr::list_modify(datasets_names, "longmin" = NULL)
-        }
-        if(!("longmax" %in% names(cl))){
-          datasets_names <- purrr::list_modify(datasets_names, "longmax" = NULL)
-        }
-        if(!("elevmin" %in% names(cl))){
-          datasets_names <- purrr::list_modify(datasets_names, "elevmin" = NULL)
-        }
-        if(!("elevmax" %in% names(cl))){
-          datasets_names <- purrr::list_modify(datasets_names, "elevmax" = NULL)
+        if(!("elev" %in% names(calls_list))){
+          datasets_names <- purrr::list_modify(datasets_names, "elev" = NULL)
         }
         
         ch_dataset <- Reduce(intersect, datasets_names)
         datasets <- append(datasets, ch_dataset)
-        
-        #datasets <- datasets[datasets != 0]
   
         if(length(datasets) != 0){
           if(!(0 %in% datasets)){
             datasets_list <- new('datasets', datasets = datasets)}
-        }else{datasets_list <- NULL}
+        }else{
+          datasets_list <- NULL}
           }
       if(!is.null(datasets_list)){
         collunit <- x@sites[[i]]@collunits@collunits
@@ -162,10 +153,11 @@ filter.default <- function(x, ...) {
       sites <- append(sites, new_site)
       
     }
-    
   }
+  
   if(!is.null(sites)){
     sites <- new('sites', sites = sites)}
+  
   
   if(exists("sites")){
     return(sites)}else{return(message("No datasets match your conditions."))}
