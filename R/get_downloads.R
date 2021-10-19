cf603eacaa98b6ff57a0ec6102e4e878f61b259d
sedv8808 <sedv8808@gmail.com>
2021-10-19 14:22:31 -0700

    API fixes
    Applied new API's structure to get_downloads object
    
    Still wondering about how to return the sites object with all new tables.

--- R/get_downloads.R
+++ R/get_downloads.R
@@ -44,26 +44,37 @@ parse_download <- function(result) {
     
     # Sites 
     # Sitename
-    sitename <- result$data[[i]]$record$data$dataset$site$sitename
+    sitename <- result$data[[i]]$dataset$data$dataset$site$sitename
     
     # Site ID
-    siteid <- result$data[[i]]$record$data$dataset$site$siteid
+    siteid <- result$data[[i]]$dataset$data$dataset$site$siteid
     
     # Location
-    location <- st_read(result$data[[i]]$record$data$dataset$site$geography, quiet = TRUE)
+    location <- result$data[[i]]$dataset$data$dataset$site$geography
+    if(is.null(location)){
+      location <- st_sf(st_sfc())
+    }else{
+      location <- st_read(result$data[[i]]$dataset$data$dataset$site$geography, quiet = TRUE)
+    }
+
 
     # Altitude
-    elev <- result$data[[i]]$record$data$dataset$site$altitude
+    elev <- result$data[[i]]$dataset$data$dataset$site$altitude
 
     # Description 
-    description <- result$data[[i]]$record$data$dataset$site$sitedescription
+    description <- result$data[[i]]$dataset$data$dataset$site$sitedescription
+    if(is.na(description)){
+      description <- NA_character_
+    }else{
+      description <- result$data[[i]]$dataset$data$dataset$site$sitedescription
+    }
 
     # Notes
     notes <- NA_character_
 
   # Datasets
-    datasetid <- result$data[[i]]$record$data$dataset$site$collectionunit$dataset$datasetid
-    datasettype <- result$data[[i]]$record$data$dataset$site$collectionunit$dataset$datasettype
+    datasetid <- result$data[[i]]$dataset$data$dataset$site$collectionunit$dataset$datasetid
+    datasettype <- result$data[[i]]$dataset$data$dataset$site$collectionunit$dataset$datasettype
     datasetnotes <- NA_character_
     
     new_dataset <- new('dataset',
@@ -78,12 +89,12 @@ parse_download <- function(result) {
 
   ## Collunits
   # Coll Unit ID
-  collunitid <- result$data[[i]]$record$data$dataset$site$collectionunit$collectionunitid
+  collunitid <- result$data[[i]]$dataset$data$dataset$site$collectionunit$collectionunitid
 
   colldate = as.Date("2007-02-01")
 
   # Coll Unit Handle
-  handle <- result$data[[i]]$record$data$dataset$site$collectionunit$handle
+  handle <- result$data[[i]]$dataset$data$dataset$site$collectionunit$handle
 
   new_collunit <- new("collunit",
                       collunitid = collunitid,
@@ -104,110 +115,110 @@ parse_download <- function(result) {
                   collunits = coll_units)
 
   sites <- append(sites, new_site)
-  
+  }
   
   
   # PI Information
-  pi_length <- length(result$data[[1]]$record$data$dataset$site$collectionunit$dataset$datasetpi[[1]])
+  #pi_length <- length(result$data[[1]]$dataset$data$dataset$site$collectionunit$dataset$datasetpi[[1]])
   
-  for(j in range(1:pi_length)){
-    pi <- result$data[[1]]$record$data$dataset$site$collectionunit$dataset$datasetpi[[1]]$contactname
-    pi_list <- append(pi_list, pi)}
-  
- 
-  
-  # Count Samples metadata
-  
-  if (result$data[[i]]$record$data$dataset == 'geochronologic') {
-    
-    message(paste0('The dataset ID ', dataset$dataset.meta$dataset.id,
-                   ' is associated with a geochronology object, not count data.'))
-    return(NULL)
-    
-  } else {
-    
-    # copy to make indexing below easier?
-    samples <- result$data[[i]]$record$data$samples
-    
-    
-    # Build the metadata for each sample in the dataset.
-    sample.meta <- do.call(rbind.data.frame,
-                           lapply(samples, `[`,
-                                  c("depth",
-                                    "sampleid"
-                                  )))
-    
-    
-    #samples <- fromJSON(samples)
-    
-    #depth <- modify_depth(alex_samples, 1, "depth") %>% as_vector()
-    
-    
-    # Taxon Table
-    length_datum <- length(result$data[[i]]$record$data$samples)
-    
-    for(j in 1:length_datum){
-      depth <- result$data[[i]]$record$data$samples[[j]]$depth
-      sample_id <-result$data[[i]]$record$data$samples[[j]]$sampleid
-      dataset_id <- result$data[[i]]$datasetid
-      df <- result$data[[i]]$record$data$samples[[j]]$datum %>% 
-        map(function(x){as.data.frame(x)}) %>% 
-        bind_rows()
-      
-      df_sample <- df %>%
-        select(variablename, units, element, taxongroup, ecologicalgroup, taxonid) # uncomment for order selection
-      
-      df1 <- rbind(df1, df_sample) %>%
-        distinct()
-       
-      # Counts
-      df_count <- df %>%
-        select(variablename, value, taxonid)
-      
-      df_counts <- rbind(df_counts, df_count) %>%
-        distinct()
-      
-      # Lab Data
-      df_lab <- result$data[[i]]$record$data$samples[[j]]$datum %>% map(function(x){as.data.frame(x)}) %>% bind_rows() %>%
-        select(taxonid)
-      
-      df_lab1 <- rbind(df_lab, df_lab1) %>%
-        distinct()
-      
-      # Sample.Meta Table
-      df2 <- result$data[[i]]$record$data$samples[[j]]$ages %>% map(function(x){as.data.frame(x)}) %>% bind_rows()
-      df2 <- df2 %>%
-        mutate(
-          depth = depth,
-          sample.id = sample_id,
-          datasetid = dataset_id) %>%
-        select(depth, ageolder, age, ageyounger, chronologyname, agetype, chronologyid, sample.id, datasetid)
-      
-      df3 <- rbind(df3, df2)
-    }
-    
-    # Chronologies filtering
-    wang <- df3 %>%
-      filter(chronologyname == "Wang et al.")
-    
-    cohmap <- df3 %>%
-      filter(chronologyname == "COHMAP chron 2")
-    
-    napd <- df3 %>%
-      filter(chronologyname == "NAPD 1")
-  }  
-
-  } 
-  
-  # DOUBT TODO : How should I save these tables?
-  #print(pi_list)
-  print(df1)
-  print(df_counts)
-  #print(df_lab1)
-  #print(df3)
-  #print(napd)
-  #print(cohmap)
-  #print(wang)
+  # for(j in range(1:pi_length)){
+  #   pi <- result$data[[1]]$dataset$data$dataset$site$collectionunit$dataset$datasetpi[[1]]$contactname
+  #   pi_list <- append(pi_list, pi)}
+  # 
+  # 
+  # 
+  # # Count Samples metadata
+  # 
+  # if (result$data[[i]]$dataset$data$dataset$site$collectionunit$dataset$datasettype == 'geochronologic') {
+  #   
+  #   message(paste0('The dataset ID ', dataset$dataset.meta$dataset.id,
+  #                  ' is associated with a geochronology object, not count data.'))
+  #   return(NULL)
+  #   
+  # } else {
+  #   
+  #   # copy to make indexing below easier?
+  #   samples <- result$data[[i]]$dataset$data$samples
+  #   
+  #   
+  #   # Build the metadata for each sample in the dataset.
+  #   sample.meta <- do.call(rbind.data.frame,
+  #                          lapply(samples, `[`,
+  #                                 c("depth",
+  #                                   "sampleid"
+  #                                 )))
+  #   
+  #   
+  #   #samples <- fromJSON(samples)
+  #   
+  #   #depth <- modify_depth(alex_samples, 1, "depth") %>% as_vector()
+  #   
+  #   
+  #   # Taxon Table
+  #   length_datum <- length(result$data[[i]]$dataset$data$samples)
+  #   
+  #   for(j in 1:length_datum){
+  #     depth <- result$data[[i]]$dataset$data$samples[[j]]$depth
+  #     sample_id <-result$data[[i]]$dataset$data$samples[[j]]$sampleid
+  #     dataset_id <- result$dataset$data[[i]]$dataset$site$siteid
+  #     df <- result$data[[i]]$dataset$data$samples[[j]]$datum %>% 
+  #       map(function(x){as.data.frame(x)}) %>% 
+  #       bind_rows()
+  #     
+  #     df_sample <- df %>%
+  #       select(variablename, units, element, taxongroup, ecologicalgroup, taxonid) # uncomment for order selection
+  #     
+  #     df1 <- rbind(df1, df_sample) %>%
+  #       distinct()
+  #      
+  #     # Counts
+  #     df_count <- df %>%
+  #       select(variablename, value, taxonid)
+  #     
+  #     df_counts <- rbind(df_counts, df_count) %>%
+  #       distinct()
+  #     
+  #     # Lab Data
+  #     df_lab <- result$data[[i]]$dataset$data$samples[[j]]$datum %>% map(function(x){as.data.frame(x)}) %>% bind_rows() %>%
+  #       select(taxonid)
+  #     
+  #     df_lab1 <- rbind(df_lab, df_lab1) %>%
+  #       distinct()
+  #     
+  #     # Sample.Meta Table
+  #     df2 <- result$data[[i]]$dataset$data$samples[[j]]$ages %>% map(function(x){as.data.frame(x)}) %>% bind_rows()
+  #     df2 <- df2 %>%
+  #       mutate(
+  #         depth = depth,
+  #         sample.id = sample_id,
+  #         datasetid = dataset_id) %>%
+  #       select(depth, ageolder, age, ageyounger, chronologyname, agetype, chronologyid, sample.id, datasetid)
+  #     
+  #     df3 <- rbind(df3, df2)
+  #   }
+  #   
+  #   # Chronologies filtering
+  #   wang <- df3 %>%
+  #     filter(chronologyname == "Wang et al.")
+  #   
+  #   cohmap <- df3 %>%
+  #     filter(chronologyname == "COHMAP chron 2")
+  #   
+  #   napd <- df3 %>%
+  #     filter(chronologyname == "NAPD 1")
+  # }  
+  # 
+  # } 
+  # 
+  # # DOUBT TODO : How should I save these tables?
+  # #print(pi_list)
+  # print(df1)
+  # print(df_counts)
+  # #print(df_lab1)
+  # #print(df3)
+  # #print(napd)
+  # #print(cohmap)
+  # #print(wang)
   
   # Convert to sites element
   sites <- new('sites', sites = sites) 
@@ -241,6 +252,7 @@ get_downloads.numeric <- function(datasetid, ...) {
   baseURL <- paste0('data/downloads/', dataset)
   
   result <- parseURL(baseURL)
+  print(result)
   
   output <- parse_download(result)
   
