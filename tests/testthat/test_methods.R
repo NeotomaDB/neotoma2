1bc371ed0c7039c6d508df2fb21aefe78ef1a2b5
SimonGoring <goring@wisc.edu>
2022-02-11 10:42:14 -0800

    Updated the UML diagram & rebuilt with a test.

--- tests/testthat/test_methods.R
+++ tests/testthat/test_methods.R
@@ -7,8 +7,8 @@ context("Run Neotoma examples only when not on CRAN")
 test_that("Working with sites.", {
   ## we don't want this to run on CRAN
   skip_on_cran()
-  
-  brazil <- '{"type": "Polygon", 
+
+  brazil <- '{"type": "Polygon",
             "coordinates": [[
                 [-73.125, -9.102],
                 [-56.953, -33.138],
@@ -16,13 +16,15 @@ test_that("Working with sites.", {
                 [-68.203, 13.923],
                 [-73.125, -9.102]
               ]]}'
-  
+
   # We can make the geojson a spatial object if we want to use the
   # functionality of the `sf` package.
   brazil_sf <- geojsonsf::geojson_sf(brazil)
-  
+
+  brazil_datasets <- get_datasets(loc = brazil[1])
+
   brazil_dl <- get_downloads(brazil_datasets)
   brazil_ds <- datasets(brazil_dl)
   brazil_cu <- collunits(brazil_dl)
-  expect_identical(brazil_ds, datasets(brazil_cu))
+  expect_identical(nrow(getids(brazil_ds)), length(datasets(brazil_cu)))
 })
