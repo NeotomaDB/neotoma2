f5c39a0c03082f5ca5279a206fb4ea41921bbe37
SimonGoring <goring@wisc.edu>
2022-11-18 10:26:49 -0800

    Linting and fixing get_download() tests.
    tests for downloads are failing because we fixed the API to use the default limit/offset methods. Needed to fix the expectations properly.

--- tests/testthat/test_download.R
+++ tests/testthat/test_download.R
@@ -15,36 +15,15 @@ test_that("get_downloads runs as expected.", {
   # functionality of the `sf` package.
   brazil_sf <- geojsonsf::geojson_sf(brazil)
 
-  brazil_datasets <- get_datasets(loc = brazil[1])
+  brazil_datasets_sf <- get_datasets(loc = brazil_sf[1], all_data = TRUE)
+  brazil_datasets <- get_datasets(loc = brazil, all_data = TRUE)
 
-  brazil_dl <- get_downloads(brazil_datasets)
+  expect_equivalent(brazil_datasets, brazil_datasets_sf)
+  brazil_dl <- get_downloads(brazil_datasets, all_data = TRUE)
   expect_identical(nrow(getids(brazil_datasets)), nrow(getids(brazil_dl)))
 
-})
-
-test_that("get_downloads yields same get_datasets ids I", {
-  ## we don't want this to run on CRAN
-  skip_on_cran()
-
-  brazil <- '{"type": "Polygon",
-            "coordinates": [[
-                [-73.125, -9.102],
-                [-56.953, -33.138],
-                [-36.563, -7.711],
-                [-68.203, 13.923],
-                [-73.125, -9.102]
-              ]]}'
-
-  # We can make the geojson a spatial object if we want to use the
-  # functionality of the `sf` package.
-  brazil_sf <- geojsonsf::geojson_sf(brazil)
-
-  brazil_datasets <- get_datasets(loc = brazil[1])
-
-  df1 <- get_datasets(loc = brazil[1]) %>% get_downloads() %>% getids()
-  df2 <- get_datasets(loc = brazil[1]) %>% getids()
-
-  expect_equal(df1, df2)
+  expect_equal(getids(brazil_datasets), 
+    getids(brazil_dl))
 
 })
 
@@ -66,5 +45,8 @@ test_that("get_downloads yields same get_datasets ids II", {
     get_datasets() %>%
     getids()
 
-  expect_equal(df1, df2)
+  # The get_downloads limit happens, so we have fewer rows in df1
+  expect_lte(nrow(df1), nrow(df2))
+  expect_true(all(df1$datasetid %in% df2$datasetid))
+
 })
