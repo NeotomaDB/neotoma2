55dc419013067e21b2e25f849daa0eb7fd0839c4
Socorro <s.dominguez@ht-data.com>
2023-04-14 18:29:33 -0700

    removed rcheck

--- neotoma2.Rcheck/00_pkg_src/neotoma2/tests/testthat/test-get_contacts.R
+++
@@ -1,9 +0,0 @@
-test_that("Non integer x returns nothing:", {
-  noint <- get_contacts(x = "Goring")
-  expect_length(noint, 1)
-})
-
-test_that("Integer x returns sets of contacts:", {
-  single <- get_contacts(x = 1)
-  expect_true(class(single) == "contacts")
-})
--- neotoma2.Rcheck/neotoma2/NEWS.md
+++
@@ -1,3 +0,0 @@
-# neotoma2 1.0.0
-
-* Added a `NEWS.md` file to track changes to the package.
