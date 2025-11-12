ce9b5eb5f5c9a21ddaf3d05db727200d45147563
Socorro Dominguez <seiryu88@icloud.com>
2025-11-11 22:15:29 -0800

    tests for c and clean passing.

--- tests/testthat/test_clean.R
+++ tests/testthat/test_clean.R
@@ -1,12 +1,13 @@
 test_that("Doubling a set of records results and cleaning results in a clean set.", {
-
-  ## we don't want this to run on CRAN
-
+  # c calls clean internally but we can call it again to be sure
   skip_on_cran()
+  alex <- get_sites(sitename = "Alex%")
+  alex2 <- get_sites(24)
+  alex_sets <- c(alex, alex2)
+  clean_al <- clean(alex_sets)
   fiftyds <- get_datasets(limit = 50)
   nextds <- get_datasets(limit = 50)
   doubled <- c(fiftyds, nextds)
-  
   testthat::expect_equal(getids(doubled), getids(nextds))
   testthat::expect_equal(length(doubled), length(fiftyds))
 })
