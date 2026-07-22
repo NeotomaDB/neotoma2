e784820458383572908d16aec97127341675ea48
hoffmanick <134728053+hoffmanick@users.noreply.github.com>
2026-07-22 10:38:49 -0500

    specified directory
    Not sure if this is necessary, but I had use_httptest() in testthat.R in order to ensure that httptest was initialized in the testing procedure, but GitHub didn't like that without specifying a directory

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,6 +2,10 @@ library(testthat)
 library(neotoma2)
 library(httptest)
 
-httptest::use_httptest()
+# Set working directory to package root for httptest
+pkg_dir <- find.package("neotoma2")
+withr::with_dir(pkg_dir, {
+  httptest::use_httptest()
+})
 
 test_check("neotoma2")
